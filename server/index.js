2b74fe30b1f89e7346a92ae6e3b87ad0f26faf31
Mantas Indrašius <mantas@indrasius.lt>
2016-11-03 13:06:34 +0200

    Accept params and start script

--- server/index.js
+++ server/index.js
@@ -2,16 +2,18 @@ const SocketCluster = require('socketcluster').SocketCluster,
   express = require('express'),
   fetch = require('node-fetch');
 
+const argv = require('minimist')(process.argv.slice(2));
+
 express()
   .get(process.env.MOUNT_POINT + '/health/deployment/test', (req, res, next) => {
     fetch(`http://127.0.0.1:${process.env.PORT}${process.env.MOUNT_POINT}/health/is_alive`)
       .then(resp => resp.ok ? res.end() : res.status(500).end())
       .catch(next);
   })
-  .listen(process.env.MANAGEMENT_PORT, () => {
+  .listen(process.env.MANAGEMENT_PORT || parseInt(argv['management-port']), () => {
     new SocketCluster({
-      port: process.env.PORT || 8080,
-      path: process.env.MOUNT_POINT || '',
+      port: process.env.PORT || parseInt(argv['port']) || 8080,
+      path: process.env.MOUNT_POINT || argv['context-path'] || '',
       appName: process.env.APP_NAME || 'dev',
       workers: 2,
       workerController: './lib/worker.js',
