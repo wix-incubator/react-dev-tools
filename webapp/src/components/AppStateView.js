03bdbd69c38fa1a6b93205cfdc80ad183e9ad0be
Mantas Indrašius <mantas@indrasius.lt>
2016-11-03 23:10:25 +0200

    Fix app state style

--- webapp/src/components/AppStateView.js
+++ webapp/src/components/AppStateView.js
@@ -12,7 +12,8 @@ export default class AppStateView extends PureComponent {
   render() {
     return (
       <div className="AppStateView">
-        <JSONTree data={this.props.appState} />
+        <h1 className="App-header">App State</h1>
+        <JSONTree data={this.props.appState} theme={appStateTheme} isLightTheme={true} />
         <button onClick={this.onClick}>Update</button>
       </div>
     );
@@ -24,3 +25,23 @@ export default class AppStateView extends PureComponent {
     });
   }
 }
+
+const appStateTheme = {
+  scheme: 'monokai',
+  base00: '#000000',
+  base01: '#383830',
+  base02: '#49483e',
+  base03: '#75715e',
+  base04: '#a59f85',
+  base05: '#f8f8f2',
+  base06: '#f5f4f1',
+  base07: '#f9f8f5',
+  base08: '#f92672',
+  base09: '#fd971f',
+  base0A: '#f4bf75',
+  base0B: '#a6e22e',
+  base0C: '#a1efe4',
+  base0D: '#66d9ef',
+  base0E: '#ae81ff',
+  base0F: '#cc6633'
+};
